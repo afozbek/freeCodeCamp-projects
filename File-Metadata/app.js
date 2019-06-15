482d6630b10c47a21cf6f9d3e1b3ebc83cef43cc
afozbek <furkanozbek1995@gmail.com>
2019-06-15 15:03:20 +0300

    handling file uploads

--- File-Metadata/app.js
+++ File-Metadata/app.js
@@ -3,7 +3,9 @@
 var express = require("express");
 var cors = require("cors");
 
+const multer = require("multer");
 // require and use "multer"...
+const upload = multer({ dest: __dirname + "/files" });
 
 var app = express();
 
@@ -14,6 +16,18 @@ app.use("/public", express.static(process.cwd() + "/public"));
 
 app.get("/", function(req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), async (req, res) => {
+  let { buffer, encoding, size, originalname, mimetype } = req.file;
+  res.json({
+    message: "File handling",
+    buffer,
+    encoding,
+    size,
+    originalname,
+    mimetype
+  });
 });
 
 app.get("/hello", function(req, res) {
