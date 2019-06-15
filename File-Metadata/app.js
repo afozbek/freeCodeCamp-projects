31098bc910582c933965ba112b10e103f897da8d
afozbek <furkanozbek1995@gmail.com>
2019-06-15 14:37:41 +0300

    initial setup for File-Metadata

---
+++ File-Metadata/app.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var express = require("express");
+var cors = require("cors");
+
+// require and use "multer"...
+
+var app = express();
+
+require("dotenv").config();
+
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.get("/hello", function(req, res) {
+  res.json({ greetings: "Hello, API" });
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Node.js listening ...");
+});
