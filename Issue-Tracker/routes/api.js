452577da0cb3041e89261173f5dd436bcd56d8cb
afozbek <furkanozbek1995@gmail.com>
2019-06-19 15:15:11 +0300

    get handler

--- Issue-Tracker/routes/api.js
+++ Issue-Tracker/routes/api.js
@@ -16,26 +16,53 @@ const Project = require("../models/projectModel");
 const Issue = require("../models/issueModel");
 
 module.exports = function(app) {
-  // app.post("/api/project", async (req, res) => {
-  //   console.log("I am posting...");
-  //   console.log(req.body);
-  //   let newProject = new Project({
-  //     project_name: req.body.project_name,
-  //     project_desc: req.body.project_desc,
-  //     contributors: ["Furkan Ozbek", "Sena Modanlıoglu"],
-  //     created_by: req.body.created_by
-  //   });
+  app.post("/api/project", async (req, res) => {
+    console.log("I am posting...");
+    console.log(req.body);
+    let newProject = new Project({
+      project_name: req.body.project_name,
+      project_desc: req.body.project_desc,
+      contributors: ["Furkan Ozbek", "Sena Modanlıoglu"],
+      created_by: req.body.created_by
+    });
 
-  //   let result = await newProject.save();
+    let result = await newProject.save();
 
-  //   res.send("Success");
-  // });
+    res.send("Success");
+  });
 
   app
     .route("/api/issues/:project")
     .get(async (req, res, next) => {
-      // TODO
-      var project = req.params.project;
+      var project_name = req.params.project;
+
+      let { open, assigned_to } = req.query;
+
+      let findParam = (err, res) => {};
+
+      if (open && assigned_to) {
+        findParam = { open, assigned_to };
+      } else if (open) {
+        // let bool = open === "true" ? true : false;
+        findParam = { open };
+      } else {
+        findParam = { assigned_to };
+      }
+      try {
+        let project = Project.findOne({ project_name });
+        if (!project) {
+          let err = new Error("Project name is not exists");
+          return next(err);
+        }
+        let issues = await Issue.find(findParam);
+
+        res.json({
+          message: "Success",
+          issues
+        });
+      } catch (err) {
+        next(err);
+      }
     })
 
     .post(async (req, res, next) => {
@@ -76,14 +103,71 @@ module.exports = function(app) {
       }
     })
 
-    .put(function(req, res) {
+    .put(async (req, res, next) => {
       // TODO
-      var project = req.params.project;
+      console.log(req.body);
+      let {
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to,
+        status_text
+      } = req.body;
+      try {
+        let project_name = await Project.findOne({
+          project_name: req.params.project
+        });
+        if (!project_name) {
+          console.log("Project doesnt exist");
+          return res.json({
+            message: "Cannot update" + req.body._id
+          });
+        }
+
+        const prevIssue = await Issue.findById(req.body._id);
+        prevIssue.issue_title = issue_title;
+        prevIssue.issue_text = issue_text;
+        prevIssue.created_by = created_by;
+        prevIssue.assigned_to = assigned_to;
+        prevIssue.status_text = status_text;
+        prevIssue.updated_on = Date.now();
+
+        const result = await prevIssue.save();
+        console.log("Result", result);
+        return res.json({
+          message: "Successfully updated",
+          updated_on: result.updated_on
+        });
+      } catch (err) {
+        err = new Error("could not update " + req.body._id);
+        return next(err);
+      }
     })
 
-    .delete(function(req, res) {
+    .delete(async (req, res) => {
       // TODO
-      var project = req.params.project;
+      try {
+        let project_name = await Project.findOne({
+          project_name: req.params.project
+        });
+        if (!project_name) {
+          console.log("Project doesnt exist");
+          return res.json({
+            message: "Cannot update" + req.body._id
+          });
+        }
+
+        let result = await Issue.findByIdAndDelete(req.body._id);
+
+        console.log("Result", result);
+        return res.json({
+          message: "Successfully deleted",
+          _id: result._id
+        });
+      } catch (err) {
+        err = new Error("could not delete " + req.body._id);
+        return next(err);
+      }
     });
 
   app.route("/report-xss-violation").get((req, res) => {
