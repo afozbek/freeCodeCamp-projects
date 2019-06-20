5e2fc43bce43eae59fa9e9c22b29507ae6ca9aab
afozbek <furkanozbek1995@gmail.com>
2019-06-20 12:44:34 +0300

    updating Model

--- Personal-Library/models/bookModel.js
+++ Personal-Library/models/bookModel.js
@@ -8,7 +8,8 @@ const bookSchema = new Schema({
     required: true,
     unique: true
   },
-  comments: [String]
+  comments: [String],
+  num_of_comments: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model("Book", bookSchema);
