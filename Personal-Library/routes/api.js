fb17e3032bad5db8cf5b4e0709f3495065444cfd
afozbek <furkanozbek1995@gmail.com>
2019-06-20 12:45:09 +0300

    adding get & post api

--- Personal-Library/routes/api.js
+++ Personal-Library/routes/api.js
@@ -1,53 +1,64 @@
-/*
-*
-*
-*       Complete the API routing below
-*       
-*       
-*/
+var expect = require("chai").expect;
 
-'use strict';
-
-var expect = require('chai').expect;
-var MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectId;
-const MONGODB_CONNECTION_STRING = process.env.DB;
+const Book = require("../models/bookModel");
 //Example connection: MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {});
 
-module.exports = function (app) {
-
-  app.route('/api/books')
-    .get(function (req, res){
+module.exports = function(app) {
+  app
+    .route("/api/books")
+    .get(async (req, res, next) => {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      try {
+        const allBooks = await Book.find().select("-__v");
+        if (!allBooks) {
+          return res.send("No books exist");
+        }
+        return res.json([...allBooks]);
+      } catch (err) {
+        err = new Error("Cannot get books");
+        return next(err);
+      }
     })
-    
-    .post(function (req, res){
-      var title = req.body.title;
+
+    .post(async (req, res, next) => {
+      const title = req.body.title;
+      let newBook = new Book({
+        title
+      });
+      try {
+        const result = await newBook.save();
+        return res.status(201).json({
+          _id: result._id,
+          title: result.title,
+          comments: result.comments
+        });
+      } catch (err) {
+        err = new Error("Post failed");
+        return next(err);
+      }
       //response will contain new book object including atleast _id and title
     })
-    
-    .delete(function(req, res){
+
+    .delete(function(req, res) {
       //if successful response will be 'complete delete successful'
     });
 
-
-
-  app.route('/api/books/:id')
-    .get(function (req, res){
+  app
+    .route("/api/books/:id")
+    .get(function(req, res) {
       var bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
-    
-    .post(function(req, res){
+
+    .post(function(req, res) {
       var bookid = req.params.id;
       var comment = req.body.comment;
       //json res format same as .get
     })
-    
-    .delete(function(req, res){
+
+    .delete(function(req, res) {
       var bookid = req.params.id;
       //if successful response will be 'delete successful'
     });
-  
 };
