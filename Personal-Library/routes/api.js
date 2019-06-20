b2427bbb860dcc657e5424f06805f4af03e7679c
afozbek <furkanozbek1995@gmail.com>
2019-06-20 14:42:11 +0300

    adding handlers

--- Personal-Library/routes/api.js
+++ Personal-Library/routes/api.js
@@ -14,7 +14,8 @@ module.exports = function(app) {
         if (!allBooks) {
           return res.send("No books exist");
         }
-        return res.json([...allBooks]);
+
+        return res.json(resBooks);
       } catch (err) {
         err = new Error("Cannot get books");
         return next(err);
@@ -40,25 +41,76 @@ module.exports = function(app) {
       //response will contain new book object including atleast _id and title
     })
 
-    .delete(function(req, res) {
+    .delete(async (req, res, next) => {
       //if successful response will be 'complete delete successful'
+      try {
+        const result = await Book.deleteMany({});
+        res.json({
+          message: "Successfully deleted all records",
+          result: `Deleted count: ${result.deletedCount}`
+        });
+      } catch (err) {
+        err = new Error("Deleting all records failed");
+        return next(err);
+      }
     });
 
   app
     .route("/api/books/:id")
-    .get(function(req, res) {
-      var bookid = req.params.id;
+    .get(async (req, res, next) => {
+      const bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      try {
+        const book = await Book.findById(bookid).select(
+          "-num_of_comments -__v"
+        );
+        if (!book) {
+          return res.send("book does not exist in db");
+        }
+        return res.json({
+          ...book._doc
+        });
+      } catch (err) {
+        return next(new Error(`Cannot get ${bookid}`));
+      }
     })
 
-    .post(function(req, res) {
+    .post(async (req, res, next) => {
       var bookid = req.params.id;
       var comment = req.body.comment;
       //json res format same as .get
+      try {
+        const updatedBook = await Book.findOneAndUpdate(
+          { _id: bookid },
+          {
+            $push: { comments: comment },
+            $inc: { num_of_comments: 1 }
+          },
+          { new: true }
+        );
+        let { _id, title, comments } = updatedBook._doc;
+
+        return res.json({
+          id: _id,
+          title: title,
+          comments: comments,
+          num_of_comments: comments.length
+        });
+      } catch (err) {
+        return next(new Error("Cannot post comment"));
+      }
     })
 
-    .delete(function(req, res) {
+    .delete(async (req, res, next) => {
       var bookid = req.params.id;
       //if successful response will be 'delete successful'
+      try {
+        await Book.deleteOne({ _id: bookid });
+        res.json({
+          message: "Delete successfull"
+        });
+      } catch (err) {
+        return next(new Error("Failed to delete a book " + bookid));
+      }
     });
 };
