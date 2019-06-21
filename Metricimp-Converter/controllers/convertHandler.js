df83bf4e10a2105f38f8d121dbd44d3756ba1c2d
afozbek <furkanozbek1995@gmail.com>
2019-06-21 18:25:59 +0300

    string functionality

--- Metricimp-Converter/controllers/convertHandler.js
+++ Metricimp-Converter/controllers/convertHandler.js
@@ -1,52 +1,42 @@
-/*
-*
-*
-*       Complete the handler logic below
-*       
-*       
-*/
-
 function ConvertHandler() {
-  
   this.getNum = function(input) {
     var result;
-    
+
     return result;
   };
-  
+
   this.getUnit = function(input) {
     var result;
-    
+
     return result;
   };
-  
+
   this.getReturnUnit = function(initUnit) {
     var result;
-    
+
     return result;
   };
 
   this.spellOutUnit = function(unit) {
     var result;
-    
+
     return result;
   };
-  
+
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     var result;
-    
+
     return result;
   };
-  
+
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
     var result;
-    
+    result = `${initNum} ${initUnit} converts to ${returnNum} ${returnUnit}`;
     return result;
   };
-  
 }
 
 module.exports = ConvertHandler;
