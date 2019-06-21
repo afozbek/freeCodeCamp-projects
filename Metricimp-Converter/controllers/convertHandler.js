95f785992ec7547f62e796b863002570f29f5e32
afozbek <furkanozbek1995@gmail.com>
2019-06-21 20:15:27 +0300

    convert handler

--- Metricimp-Converter/controllers/convertHandler.js
+++ Metricimp-Converter/controllers/convertHandler.js
@@ -1,35 +1,59 @@
 function ConvertHandler() {
-  this.getNum = function(input) {
-    var result;
+  this.getNumAndUnit = function(input) {
+    let isChar = /[a-z]/i;
+    let initNum = "";
+    let initUnit = "";
 
-    return result;
-  };
-
-  this.getUnit = function(input) {
-    var result;
-
-    return result;
+    for (let char of input) {
+      if (isChar.test(char)) {
+        initUnit += char;
+      } else {
+        initNum += char;
+      }
+    }
+    return [initNum, initUnit];
   };
 
   this.getReturnUnit = function(initUnit) {
-    var result;
-
-    return result;
-  };
-
-  this.spellOutUnit = function(unit) {
-    var result;
-
-    return result;
+    switch (initUnit) {
+      case "gal":
+        return "L";
+      case "L":
+        return "gal";
+      case "lbs":
+        return "kg";
+      case "kg":
+        return "lbs";
+      case "mi":
+        return "km";
+      case "km":
+        return "mi";
+      default:
+        return null;
+    }
   };
 
   this.convert = function(initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    var result;
+    const galToL = 3.78541; // 1 gal
+    const lbsToKg = 0.453592; // 1 lbs
+    const miToKm = 1.60934; // 1 mi
 
-    return result;
+    switch (initUnit) {
+      case "gal":
+        return +initNum * galToL;
+      case "L":
+        return +initNum / galToL;
+      case "lbs":
+        return +initNum * lbsToKg;
+      case "kg":
+        return +initNum / lbsToKg;
+      case "mi":
+        return +initNum * miToKm;
+      case "km":
+        return +initNum / miToKm;
+      default:
+        return null;
+    }
   };
 
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
