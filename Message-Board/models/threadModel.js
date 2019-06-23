466ae47851446a06ef1d7888d466b9d72d225230
afozbek <furkanozbek1995@gmail.com>
2019-06-23 16:53:33 +0300

    updating board_model

--- Message-Board/models/threadModel.js
+++ Message-Board/models/threadModel.js
@@ -2,22 +2,24 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const threadSchema = new Schema({
-  text: {
-    type: String,
-    required: true
+const threadSchema = new Schema(
+  {
+    text: {
+      type: String,
+      required: true
+    },
+    board_name: String,
+    delete_password: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    replies: {
+      type: [Object]
+    },
+    reported: Boolean
   },
-  delete_password: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  replies: {
-    type: [String]
-  },
-  created_on: { type: Date, default: Date.now },
-  bumped_on: { type: Date, default: Date.now },
-  reported: Boolean
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Thread", threadSchema);
