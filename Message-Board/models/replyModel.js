f41d078f0ea56942ed40244cad63231df602d8ab
afozbek <furkanozbek1995@gmail.com>
2019-06-23 17:23:48 +0300

    working on replyModel

--- Message-Board/models/replyModel.js
+++ Message-Board/models/replyModel.js
@@ -3,10 +3,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const replySchema = new Schema({
+  // TODO
   text: {
     type: String,
     required: true
   },
+  thread_id: { type: Schema.Types.ObjectId, ref: "Thread" },
   delete_password: {
     type: String,
     required: true,
