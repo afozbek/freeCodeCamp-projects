9050d5559ce1ed5447c57d59adfa6b777bb2cdf3
afozbek <furkanozbek1995@gmail.com>
2019-06-23 12:31:35 +0300

    clearing api

--- Stock-Checker/routes/api.js
+++ Stock-Checker/routes/api.js
@@ -1,23 +1,7 @@
-/*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+"use strict";
 
-'use strict';
+var expect = require("chai").expect;
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
-
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
-
-module.exports = function (app) {
-
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
-    
+module.exports = function(app) {
+  app.route("/api/stock-prices").get(async (req, res, next) => {});
 };
