2d7f686faf7c40b5b6b8bc51e1bdec076e470957
afozbek <furkanozbek1995@gmail.com>
2019-06-24 20:07:49 +0300

    adding initial setup for functional testing

--- Message-Board/tests/2_functional-tests.js
+++ Message-Board/tests/2_functional-tests.js
@@ -1,59 +1,124 @@
 /*
-*
-*
-*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]-----
-*       (if additional are added, keep them at the very end!)
-*/
+ *
+ *
+ *       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
+ *       -----[Keep the tests in the same order!]-----
+ *       (if additional are added, keep them at the very end!)
+ */
 
-var chaiHttp = require('chai-http');
-var chai = require('chai');
+var chaiHttp = require("chai-http");
+var chai = require("chai");
 var assert = chai.assert;
-var server = require('../server');
+var server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite("Functional Tests", function() {
+  suite("API ROUTING FOR /api/threads/:board", function() {
+    suite("POST", function() {
+      test("TITLE COME HERE", done => {
+        chai
+          .request(server)
+          .post("/api/threads/Furkan3")
+          .set({ field1: "something" })
+          .end((err, res) => {
+            assert.equal(res.status, 200);
+            done();
+          });
+      });
+    });
 
-  suite('API ROUTING FOR /api/threads/:board', function() {
-    
-    suite('POST', function() {
-      
+    suite("GET", function() {
+      test("TITLE COME HERE", done => {
+        chai
+          .request(server)
+          .get("/api/threads/Furkan3")
+          .query({ field1: "something" })
+          .end((err, res) => {
+            assert.equal(res.status, 200);
+            done();
+          });
+      });
     });
-    
-    suite('GET', function() {
-      
-    });
-    
-    suite('DELETE', function() {
-      
-    });
-    
-    suite('PUT', function() {
-      
-    });
-    
 
+    suite("DELETE", function() {
+      test("TITLE COME HERE", done => {
+        chai
+          .request(server)
+          .delete("/api/threads/Furkan3")
+          .set({ field1: "something" })
+          .end((err, res) => {
+            assert.equal(res.status, 200);
+            done();
+          });
+      });
+    });
+
+    suite("PUT", function() {
+      test("TITLE COME HERE", done => {
+        chai
+          .request(server)
+          .put("/api/threads/Furkan3")
+          .set({ field1: "something" })
+          .end((err, res) => {
+            assert.equal(res.status, 200);
+            done();
+          });
+      });
+    });
   });
-  
-  suite('API ROUTING FOR /api/replies/:board', function() {
-    
-    suite('POST', function() {
-      
-    });
-    
-    suite('GET', function() {
-      
-    });
-    
-    suite('PUT', function() {
-      
-    });
-    
-    suite('DELETE', function() {
-      
-    });
-    
-  });
 
+  suite("API ROUTING FOR /api/replies/:board", function() {
+    suite("POST", function() {
+      test("TITLE COME HERE", done => {
+        chai
+          .request(server)
+          .get("/api/replies/Furkan3")
+          .set({ field1: "something" })
+          .end((err, res) => {
+            assert.equal(res.status, 200);
+            done();
+          });
+      });
+    });
+
+    suite("GET", function() {
+      test("TITLE COME HERE", done => {
+        chai
+          .request(server)
+          .get("/api/replies/Furkan3")
+          .query({ field1: "something" })
+          .end((err, res) => {
+            assert.equal(res.status, 200);
+            done();
+          });
+      });
+    });
+
+    suite("PUT", function() {
+      test("TITLE COME HERE", done => {
+        chai
+          .request(server)
+          .put("/api/replies/Furkan3")
+          .set({ field1: "something" })
+          .end((err, res) => {
+            assert.equal(res.status, 200);
+            done();
+          });
+      });
+    });
+
+    suite("DELETE", function() {
+      test("TITLE COME HERE", done => {
+        chai
+          .request(server)
+          .delete("/api/replies/Furkan3")
+          .set({ field1: "something" })
+          .end((err, res) => {
+            assert.equal(res.status, 200);
+            done();
+          });
+      });
+    });
+  });
 });
