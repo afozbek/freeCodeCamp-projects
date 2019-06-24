beb78c9e367438f4e3c8a98aca6ad4e6414ec082
afozbek <furkanozbek1995@gmail.com>
2019-06-24 17:36:25 +0300

    updating reply model

--- Message-Board/models/replyModel.js
+++ Message-Board/models/replyModel.js
@@ -2,23 +2,20 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const replySchema = new Schema({
-  // TODO
-  text: {
-    type: String,
-    required: true
+const replySchema = new Schema(
+  {
+    text: {
+      type: String,
+      required: true
+    },
+    thread_id: { type: Schema.Types.ObjectId, ref: "Thread" },
+    delete_password: {
+      type: String,
+      required: true
+    },
+    reported: Boolean
   },
-  thread_id: { type: Schema.Types.ObjectId, ref: "Thread" },
-  delete_password: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  replies: {
-    type: [String]
-  },
-  created_on: { type: Date, default: Date.now },
-  reported: Boolean
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Reply", replySchema);
