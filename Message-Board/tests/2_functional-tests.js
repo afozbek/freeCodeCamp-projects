b2ecd02ac996e673a869fb654b2a4359ee62ce12
afozbek <furkanozbek1995@gmail.com>
2019-06-25 10:54:21 +0300

    adding functional tests

--- Message-Board/tests/2_functional-tests.js
+++ Message-Board/tests/2_functional-tests.js
@@ -1,11 +1,3 @@
-/*
- *
- *
- *       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
- *       -----[Keep the tests in the same order!]-----
- *       (if additional are added, keep them at the very end!)
- */
-
 var chaiHttp = require("chai-http");
 var chai = require("chai");
 var assert = chai.assert;
@@ -15,25 +7,48 @@ chai.use(chaiHttp);
 
 suite("Functional Tests", function() {
   suite("API ROUTING FOR /api/threads/:board", function() {
-    suite("POST", function() {
-      test("TITLE COME HERE", done => {
+    suite("GET", function() {
+      test("GET ALL THREADS", done => {
         chai
           .request(server)
-          .post("/api/threads/Furkan3")
-          .set({ field1: "something" })
+          .get("/api/threads/Furkan3")
           .end((err, res) => {
+            assert.isArray(res.body, "Is res.body array?");
             assert.equal(res.status, 200);
             done();
           });
       });
     });
 
-    suite("GET", function() {
-      test("TITLE COME HERE", done => {
+    suite("POST", function() {
+      test("ADD ALL REQUIRED FIELDS", done => {
         chai
           .request(server)
-          .get("/api/threads/Furkan3")
-          .query({ field1: "something" })
+          .post("/api/threads/Furkan3")
+          .send({ text: "Yeni bir thread5", delete_password: "1234" })
+          .end((err, res) => {
+            assert.equal(res.status, 200);
+            done();
+          });
+      });
+      test("ADD SOME REQUIRED FIELDS", done => {
+        chai
+          .request(server)
+          .post("/api/threads/Furkan3")
+          .send({ text: "Yeni bir thread3" })
+          .end((err, res) => {
+            assert.equal(res.status, 500);
+            done();
+          });
+      });
+    });
+
+    suite("PUT", function() {
+      test("UPDATE THREAD", done => {
+        chai
+          .request(server)
+          .put("/api/threads/Furkan3")
+          .send({ thread_id: "5d11cade8e7256265c98b2e2" })
           .end((err, res) => {
             assert.equal(res.status, 200);
             done();
@@ -42,26 +57,28 @@ suite("Functional Tests", function() {
     });
 
     suite("DELETE", function() {
-      test("TITLE COME HERE", done => {
+      test("DELETE A THREAD WITH ALL REQUIRED FIELDS", done => {
         chai
           .request(server)
           .delete("/api/threads/Furkan3")
-          .set({ field1: "something" })
+          .send({
+            thread_id: "5d11cae18e7256265c98b2e3",
+            delete_password: "1234"
+          })
           .end((err, res) => {
             assert.equal(res.status, 200);
             done();
           });
       });
-    });
 
-    suite("PUT", function() {
-      test("TITLE COME HERE", done => {
+      test("DELETE A THREAD WITH SOME REQUIRED FIELDS", done => {
         chai
           .request(server)
-          .put("/api/threads/Furkan3")
-          .set({ field1: "something" })
+          .delete("/api/threads/Furkan3")
+          .send({ thread_id: "5d11cae18e7256265c98b2e3" })
           .end((err, res) => {
-            assert.equal(res.status, 200);
+            assert.equal(res.status, 500);
+            assert.notEqual(res.body, "Delete successfull");
             done();
           });
       });
@@ -70,37 +87,57 @@ suite("Functional Tests", function() {
 
   suite("API ROUTING FOR /api/replies/:board", function() {
     suite("POST", function() {
-      test("TITLE COME HERE", done => {
+      test("POST A REPLY TO THREAD WITH ALL REQUIRED FIELDS", done => {
         chai
           .request(server)
-          .get("/api/replies/Furkan3")
-          .set({ field1: "something" })
+          .post("/api/replies/Furkan3")
+          .send({
+            thread_id: "5d10ff252ec6342238903b1f",
+            delete_password: "1234",
+            text: "yeni bir reply"
+          })
           .end((err, res) => {
             assert.equal(res.status, 200);
+            done();
+          });
+      });
+      test("POST A REPLY TO THREAD WITH SOME REQUIRED FIELDS", done => {
+        chai
+          .request(server)
+          .post("/api/replies/Furkan3")
+          .send({
+            thread_id: "5d10ff252ec6342238903b1f"
+          })
+          .end((err, res) => {
+            assert.equal(res.status, 500);
             done();
           });
       });
     });
 
     suite("GET", function() {
-      test("TITLE COME HERE", done => {
+      test("GETTING ENTIRE THREAD WORKS", done => {
         chai
           .request(server)
           .get("/api/replies/Furkan3")
-          .query({ field1: "something" })
+          .query({ thread_id: "5d10ff252ec6342238903b1f" })
           .end((err, res) => {
             assert.equal(res.status, 200);
+            assert.equal(res.body.message, "Yep we got the threads");
             done();
           });
       });
     });
 
     suite("PUT", function() {
-      test("TITLE COME HERE", done => {
+      test("UPDATING REPLY WORKS", done => {
         chai
           .request(server)
           .put("/api/replies/Furkan3")
-          .set({ field1: "something" })
+          .send({
+            reply_id: "5d11c90599fff71ad80d8d90",
+            thread_id: "5d10ff252ec6342238903b1f"
+          })
           .end((err, res) => {
             assert.equal(res.status, 200);
             done();
@@ -109,11 +146,15 @@ suite("Functional Tests", function() {
     });
 
     suite("DELETE", function() {
-      test("TITLE COME HERE", done => {
+      test("SET TEXT [DELETED] WORKS", done => {
         chai
           .request(server)
           .delete("/api/replies/Furkan3")
-          .set({ field1: "something" })
+          .send({
+            reply_id: "5d11c90599fff71ad80d8d90",
+            thread_id: "5d10ff252ec6342238903b1f",
+            delete_password: "1234"
+          })
           .end((err, res) => {
             assert.equal(res.status, 200);
             done();
